5269de8913dd2d2af189d346c57b6fcfb516fd28
jaRue1 <jaruejohnson@outlook.com>
2022-05-02 03:03:11 -0400

    setup main branch

--- script.js
+++ script.js
@@ -1,26 +1,15 @@
-const HOURHAND = document.querySelector("#hour")
-const MINUTEHAND = document.querySelector("#minute")
-const SECONDHAND = document.querySelector("#second")
+// Be sure to reference the ReadMe and the Helpful Links
+// Happy Coding !
 
-var date = new Date()
-console.log(date)
-let hr = date.getHours()
-let min = date.getMinutes()
-let sec = date.getSeconds()
-console.log("Hour: " + hr + " Minute: " + min + " Second: " + sec)
+// Select each hand of the clock using document.querySelector
 
-let hrPosition = (hr * 360) / 12 + (min * (360 / 60)) / 12
-let minPosition = (min * 360) / 60 + (sec * (360 / 60)) / 60
-let secPosition = (sec * 360) / 60
+// grab the current date using the Date() method and
+// seperate the time into hours, minutes and seconds
 
-function runTheClock() {
-  hrPosition = hrPosition + 3 / 360
-  minPosition = minPosition + 6 / 60
-  secPosition = secPosition + 6
+// convert the times into 3 separate positions
 
-  HOURHAND.style.transform = "rotate(" + hrPosition + "deg)"
-  MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)"
-  SECONDHAND.style.transform = "rotate(" + secPosition + "deg)"
-}
+// define a function that will move the hands on the clock using transform
 
-var interval = setInterval(runTheClock, 1000)
+// set a an interval and pass it you function with desired params
+
+// HINT use setInterval()
